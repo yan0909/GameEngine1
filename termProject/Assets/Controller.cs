96e274b70400fd9c8fc9fef8ef0726d493098c23
Kim Seong Eun <yan0909@naver.com>
2020-07-02 00:03:18 +0900

    test move controller(walk, jump)

---
+++ termProject/Assets/Controller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody rigid;
+
+    private float jumpPower = 4;
+    private float moveSpeed = 1;
+    private float runSpeed = 2;
+
+    private bool isJumping;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        isJumping = false;
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate((new Vector3(h, 0, v) * moveSpeed) * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            transform.Translate((new Vector3(h, 0, v) * runSpeed) * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //바닥에 있으면 점프를 실행
+            if (!isJumping)
+            {
+                isJumping = true;
+                rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //바닥에 닿으면
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //점프가 가능한 상태로 만듦
+            isJumping = false;
+        }
+    }
+}
