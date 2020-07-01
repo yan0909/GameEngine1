d55a2e0469cc3376318fb5aa18137a62c9a4195a
Kim Seong Eun <yan0909@naver.com>
2020-07-02 02:51:23 +0900

    add follow camera

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 1.2f;
+    public float offsetZ = -4f;
+
+    public float followSpeed = 10;
+
+    Vector3 cameraPosition;
+
+    void LateUpdate()
+    {
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
